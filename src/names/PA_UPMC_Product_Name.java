888648557bea091b62fd0ec9606c382a46fb14f9
Xuerui <faxuerui@seas.upenn.edu>
2017-07-28 15:03:58 -0400

    Changing UPMC class to fit new structure

---
+++ src/names/PA_UPMC_Product_Name.java
@@ -0,0 +1,12 @@
+package names;
+
+import components.Main.Carrier;
+
+public class PA_UPMC_Product_Name extends Product_Name {
+
+	public PA_UPMC_Product_Name(String original_name) {
+		super(original_name);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
--- src/names/Product_Name.java
+++ src/names/Product_Name.java
@@ -24,7 +24,7 @@ public class Product_Name {
 	}
 	
 	public enum Plan {
-		Choice_Plus, EPO, PPO, HMO, POS, HSA, QPOS, Savings_Plus, Wekkspan_HNOption, LVHN_HNOption, None
+		Choice_Plus, EPO, PPO, HMO, POS, HSA, QPOS, Savings_Plus, Wellspan_HNOption, LVHN_HNOption, None
 	}
 	
 	@Override
@@ -46,30 +46,4 @@ public class Product_Name {
 			return Metal.None;
 		}
 	}
-	
-	public static Plan getPlan(String s) {
-		String str = s.toLowerCase().replaceAll("[\\s_]", "");
-		if (str.contains("choiceplus")) {
-			return Plan.Choice_Plus;
-		} else if (str.contains("epo")) {
-			return Plan.EPO;
-		} else if (str.contains("ppo")) {
-			return Plan.PPO;
-		} else if (str.contains("hmo")) {
-			return Plan.HMO;
-		} else if (str.contains("hsa")) {
-			return Plan.HSA;
-		} else if (str.contains("qpos")) {
-			return Plan.QPOS;
-		} else if (str.contains("savingsplus")) {
-			return Plan.Savings_Plus;
-		} else if (str.contains("wekkspanhnoption")) {
-			return Plan.Wekkspan_HNOption;
-		} else if (str.contains("lvhnhnoption")) { //Add more to this
-			return Plan.LVHN_HNOption;
-		} else {
-			return Plan.None;
-		}
-	}
-	
 }
